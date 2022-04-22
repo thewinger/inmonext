0b7ba570f384314860469e58b2276966f6bb3276
thewinger <winger.87@gmail.com>
2022-04-22 20:57:57 +0200

    Gallery working

--- pages/components/ProductSlider.tsx
+++ pages/components/ProductSlider.tsx
@@ -1,61 +1,58 @@
-import React, {
-  useState,
-  useEffect,
-  useCallback,
-} from 'react'
-import { Thumb } from './ProductSliderThumb'
-import useEmblaCarousel from 'embla-carousel-react'
-import Image from 'next/image'
-import Shimmer from './Shimmer'
-
+import React, { useState, useEffect, useCallback } from "react";
+import { Thumb } from "./ProductSliderThumb";
+import useEmblaCarousel from "embla-carousel-react";
+import Image from "next/image";
+import Shimmer from "./Shimmer";
 
 type PropType = {
-   slides: []
-}
+  slides: [];
+};
 
 const ProductSlider = ({ slides }: PropType) => {
-  const [selectedIndex, setSelectedIndex] = useState(0)
-  const [mainViewportRef, embla] = useEmblaCarousel({ skipSnaps: false })
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [mainViewportRef, embla] = useEmblaCarousel({ skipSnaps: false });
   const [thumbViewportRef, emblaThumbs] = useEmblaCarousel({
     containScroll: "keepSnaps",
-    dragFree: true
-  })
+    dragFree: true,
+  });
 
   const onThumbClick = useCallback(
     (index) => {
-      if (!embla || !emblaThumbs) return
-      if (emblaThumbs.clickAllowed()) embla.scrollTo(index)
+      if (!embla || !emblaThumbs) return;
+      if (emblaThumbs.clickAllowed()) embla.scrollTo(index);
     },
     [embla, emblaThumbs]
-  )
+  );
 
   const onSelect = useCallback(() => {
-    if (!embla || !emblaThumbs) return
-    setSelectedIndex(embla.selectedScrollSnap())
-    emblaThumbs.scrollTo(embla.selectedScrollSnap())
-  }, [embla, emblaThumbs, setSelectedIndex])
+    if (!embla || !emblaThumbs) return;
+    setSelectedIndex(embla.selectedScrollSnap());
+    emblaThumbs.scrollTo(embla.selectedScrollSnap());
+  }, [embla, emblaThumbs, setSelectedIndex]);
 
   useEffect(() => {
-    if (!embla) return
-    onSelect()
-    embla.on('select', onSelect)
-  }, [embla, onSelect])
-
+    if (!embla) return;
+    onSelect();
+    embla.on("select", onSelect);
+  }, [embla, onSelect]);
 
   return (
-   <div className='flex flex-col w-full'>
-      <div className="embla relative overflow-hidden m-0 block w-full rounded p-0 mb-4">
-        <div className="embla__viewport aspect-w-3 aspect-h-2" ref={mainViewportRef}>
+    <div className="flex w-full flex-col">
+      <div className="embla relative m-0 mb-4 block w-full overflow-hidden rounded p-0">
+        <div
+          className="embla__viewport aspect-w-3 aspect-h-2"
+          ref={mainViewportRef}
+        >
           <div className="embla__container flex gap-2">
             {slides.map((slide, index) => (
               <div className="embla__slide min-w-full" key={index}>
-                <div className="embla__slide__inner rounded relative overflow-hidden aspect-w-3 aspect-h-2">
+                <div className="embla__slide__inner aspect-w-3 aspect-h-2 relative overflow-hidden rounded">
                   <Image
                     className="embla__slide__img relative block flex-[0_0_100%]"
-                    src={slide['sourceUrl']}
-                    alt={slide['title']}
-                    layout='fill'
-                    placeholder='blur'
+                    src={slide["sourceUrl"]}
+                    alt={slide["title"]}
+                    layout="fill"
+                    placeholder="blur"
                     blurDataURL={Shimmer}
                     priority
                   />
@@ -66,23 +63,24 @@ const ProductSlider = ({ slides }: PropType) => {
         </div>
       </div>
 
-      <div className="embla embla--thumb relative overflow-hidden m-0 block w-full rounded p-0 mb-4">
-        <div className="embla__viewport aspect-w-3 aspect-h-2" ref={thumbViewportRef}>
-          <div className="embla__container embla__container--thumb flex gap-1">
-           {slides && slides.map((slide, index) => (
-              <Thumb
-                onClick={() => onThumbClick(index)}
-                selected={index === selectedIndex}
-                imgSrc={slide['sourceUrl']}
-                imgTitle={slide['title']}
-                key={index}
-              />
-           ))}
-           </div>
+      <div className="embla embla--thumb relative m-0  block w-full overflow-hidden p-0">
+        <div className="embla__viewport h-16 w-full" ref={thumbViewportRef}>
+          <div className="embla__container embla__container--thumb flex h-full gap-1">
+            {slides &&
+              slides.map((slide, index) => (
+                <Thumb
+                  onClick={() => onThumbClick(index)}
+                  selected={index === selectedIndex}
+                  imgSrc={slide["sourceUrl"]}
+                  imgTitle={slide["title"]}
+                  key={index}
+                />
+              ))}
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductSlider
+export default ProductSlider;
--- pages/components/ProductSliderThumb.tsx
+++ pages/components/ProductSliderThumb.tsx
@@ -1,26 +1,32 @@
 import Image from "next/image";
 import { MouseEventHandler } from "react";
 
-
 type PropType = {
-  selected: boolean,
-  onClick: MouseEventHandler<HTMLButtonElement>,
-  imgSrc: string,
-  imgTitle: string,
-}
+  selected: boolean;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  imgSrc: string;
+  imgTitle: string;
+};
 
 export const Thumb = ({ selected, onClick, imgSrc, imgTitle }: PropType) => (
   <div
-    className={`embla__slide embla__slide--thumb w-1/5 basis-1/5 ${
-      selected ? "is-selected" : ""
+    className={`embla__slide embla__slide--thumb h-full w-1/5 shrink-0 rounded opacity-50 transition-opacity ${
+      selected ? "is-selected border-2 border-green-500 opacity-100" : ""
     }`}
   >
     <button
       onClick={onClick}
-      className="embla__slide__inner embla__slide__inner--thumb block w-full rounded overflow-hidden relative h-full"
+      className="embla__slide__inner embla__slide__inner--thumb  relative h-full w-full "
       type="button"
     >
-      <Image className="embla__slide__thumbnail relative block min-w-full" src={imgSrc} alt={imgTitle} layout="fill" priority/>
+      <Image
+        className="embla__slide__thumbnail  relative block rounded"
+        src={imgSrc}
+        alt={imgTitle}
+        layout="fill"
+        objectFit="cover"
+        priority
+      />
     </button>
   </div>
 );
