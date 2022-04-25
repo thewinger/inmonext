a015e327208dbf2e3e9e149f6ac757b3cfb4ef59
thewinger <winger.87@gmail.com>
2022-04-25 08:23:36 +0200

    -

--- pages/[statustagUrl].tsx
+++ pages/[statustagUrl].tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 import { useRouter } from "next/router";
 import Layout from "./components/Layout";
 
-const Venta = () => {
+const TipoPage = (props) => {
   const router = useRouter();
   const query = router.query;
   let statustag = query.statustagUrl as string;
@@ -21,4 +21,18 @@ const Venta = () => {
   );
 };
 
-export default Venta;
+export default TipoPage;
+
+export async function getStaticPaths() {
+  const paths = ["en-venta", "en-alquiler", "obra-nueva"];
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps() {
+  return {
+    props: {},
+  };
+}
