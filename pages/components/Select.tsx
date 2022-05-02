e49048b7adf169ca882b7842fb174a964f7049a6
thewinger <winger.87@gmail.com>
2022-05-02 08:07:41 +0200

    Changes amber to green in Select

--- pages/components/Select.tsx
+++ pages/components/Select.tsx
@@ -58,7 +58,7 @@ export const Select = ({name, label, emptyFirst, placeholder, options, onChange}
                 value=""
                 className={({ active }) =>
                   `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                    active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                    active ? 'bg-green-100 text-green-900' : 'text-gray-900'
                   }`
                 }
               >
@@ -72,7 +72,7 @@ export const Select = ({name, label, emptyFirst, placeholder, options, onChange}
                     Todos
                     </span>
                     {selected ? (
-                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-green-600">
                         <CheckIcon className="h-5 w-5" aria-hidden="true" />
                       </span>
                     ) : null}
@@ -86,7 +86,7 @@ export const Select = ({name, label, emptyFirst, placeholder, options, onChange}
                   value={option.value}
                   className={({ active }) =>
                     `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                      active ? 'bg-green-100 text-green-900' : 'text-gray-900'
                     }`
                   }
                 >
@@ -100,7 +100,7 @@ export const Select = ({name, label, emptyFirst, placeholder, options, onChange}
                         {option.item}
                       </span>
                       {selected ? (
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-green-600">
                           <CheckIcon className="h-5 w-5" aria-hidden="true" />
                         </span>
                       ) : null}
